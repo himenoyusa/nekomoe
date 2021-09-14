79d7e8ac790392f4a8c2a57494eb95b9a77dd419
himenoyusa <876203144@qq.com>
2021-09-14 08:49:28 +0800

    init: change homepage

---
+++ saya.js
@@ -0,0 +1,19 @@
+var time = new Date();
+var hour = time.getHours();
+var audio = document.getElementById("load");
+
+// if (hour >= 6 && hour < 8) {
+//     audio.src = 'saya/ohayou.mp3';
+// } else if (hour >= 8 && hour < 12) {
+//     audio.src = 'saya/konniqiha.mp3';
+// } else if (hour >= 12 && hour < 14) {
+//     audio.src = 'saya/ohiruka.mp3';
+// } else if (hour >= 14 && hour < 18) {
+//     audio.src = 'saya/hisashiburi.mp3';
+// } else if (hour >= 18 && hour < 23) {
+//     audio.src = 'saya/yorudayo.mp3';
+// } else {
+//     audio.src = 'saya/yonakadane.mp3';
+// }
+
+audio.src = "saya/sena.wav";
